95ec7657867161790ade85017f70cd01a21281ac
portalnegocios25-design <portalnegocios25@gmail.com>
2026-09-21 22:40:18 -0300

    feat: criar app/layout.js

---
+++ app/layout.js
@@ -0,0 +1,1 @@
+import "./globals.css";export const metadata={title:"Austen Comercial",description:"Gestão comercial Austen"};export default function RootLayout({children}){return <html lang="pt-BR"><body>{children}</body></html>}
